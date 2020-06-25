b85ae0ed4fcd78c698d4d23a95f9a82cb6a48ef7
Trystan May <trystanmay@gmail.com>
2020-06-25 18:38:24 -0500

    Added plotting and polyfit.
    Signed-off-by: Trystan May <trystanmay@gmail.com>

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import datetime as dt
 
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
@@ -92,6 +94,51 @@ def organize_data(df: pd.DataFrame) -> pd.DataFrame:
     return pd.DataFrame(data={'time_frame': time_frames, 'price': prices})
 
 
+def predict_with_linear_model(time_frames: pd.Series, prices: pd.Series) -> float:
+    shaped_time_frames: pd.Series = time_frames.values.reshape((-1, 1))
+    shaped_prices: pd.Series = prices.values.reshape((-1, 1))
+    model: LinearRegression = LinearRegression().fit(shaped_time_frames, shaped_prices)
+
+    print(f'Model parameters: coefficients: {model.coef_}, intercepts: {model.intercept_}')
+
+    coefficients = np.poly1d([model.coef_[0][0], model.intercept_[0]])
+
+    next_time_frame = shaped_time_frames[-1] + 1
+    prediction = model.predict([next_time_frame])
+    return [next_time_frame, prediction], coefficients
+
+
+def predict_with_polyfit(time_frames: pd.Series, prices: pd.Series):
+    shaped_time_frames: pd.Series = time_frames.values.reshape(-1)
+    shaped_prices: pd.Series = prices.values.reshape(-1)
+
+    degrees: int = 2
+    poly_coefficients = np.polyfit(shaped_time_frames, shaped_prices, degrees)
+
+    print(f'Polynomial coefficients: {poly_coefficients}')
+
+    poly = np.poly1d(poly_coefficients)
+
+    next_time_frame = shaped_time_frames[-1] + 1
+    prediction = poly(next_time_frame)
+    return [next_time_frame, prediction], poly
+
+
+def plot(time_frames: pd.Series, prices: pd.Series, prediction, model_coefficients: np.ndarray):
+    min_tf: float = time_frames.min()
+    max_tf: float = time_frames.max()
+    tf_stddev: float = time_frames.std()
+
+    n: int = 100
+
+    dummy_x: np.ndarray = np.linspace(min_tf - tf_stddev, max_tf + tf_stddev, n)
+
+    plt.plot(time_frames, prices, '.',
+             dummy_x, model_coefficients(dummy_x), '-',
+             prediction[0], prediction[1], '*')
+    plt.show()
+
+
 def predict_turnip_prices(filepath: str) -> float:
     """
     Predicts the stalk market prices for the next time frame with the data at the provided file path.
@@ -101,19 +148,26 @@ def predict_turnip_prices(filepath: str) -> float:
     df_raw: pd.DataFrame = get_data(filepath)
     df: pd.DataFrame = organize_data(df_raw)
 
-    time_frames = df['time_frame'].values.reshape((-1, 1))
-    prices = df['price'].values.reshape((-1, 1))
-
-    model: LinearRegression = LinearRegression().fit(time_frames, prices)
+    time_frames = df['time_frame']
+    prices = df['price']
 
     data_print = '\n'.join([f'{time_frames[i]}: {prices[i]}' for i in range(len(time_frames))])
     print(f'Input data: \n{data_print}')
-    print(f'Model being used: {model.__class__.__name__}')
-    print(f'Model parameters: coefficients: {model.coef_}, intercepts: {model.intercept_}')
 
-    last_time_frame = time_frames[-1] + 1
-    prediction = model.predict([last_time_frame])
-    return prediction[0]
+    print('How would you like to predict the next turnip price?')
+    model_input: str = input('[L]inear Regression, [P]olyfit\n')[0].lower()
+
+    if model_input == 'l':
+        prediction, model = predict_with_linear_model(time_frames, prices)
+    elif model_input == 'p':
+        prediction, model = predict_with_polyfit(time_frames, prices)
+    else:
+        print('Invalid model type.')
+        return 0.0
+
+    plot(time_frames, prices, prediction, model)
+
+    return prediction
 
 
 if __name__ == '__main__':
